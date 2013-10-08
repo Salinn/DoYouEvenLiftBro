a1c6da9725d0549d2194e9ef64174ba0b16465a3
nmc2484 <nsamachipalo@gmail.com>
2013-10-07 21:02:07 -0400

    starting the class interface

---
+++ src/classInterface.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class classInterface {
+    private JPanel classPanel;
+    private JPanel bottomPanel;
+    private JButton addClass;
+    private JButton editClass;
+
+    public classInterface(){
+        classPanel = new JPanel();
+
+
+
+    }
+
+}
